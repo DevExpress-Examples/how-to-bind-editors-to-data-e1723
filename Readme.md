f1525bf637429b0dbe545c3542c093e9d64b3f0d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to bind editors to data
+
+
+<p>This example shows how to bind the TextEdit, SpinEdit and ComboBoxEdit to data obtained from an MS Access database.</p>
+
+<br/>
+
+
---
+++ VB/DXEditors_DataBinding/Window1.xaml.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace DXEditors_DataBinding
+	Partial Public Class Window1
+		Inherits Window
+		Private dt As DXEditors_DataBinding.nwindDataSet.ProductsDataTable
+		Private currentRecord As Integer
+		Public Sub New()
+			InitializeComponent()
+			currentRecord = 0
+			dt = New nwindDataSetTableAdapters.ProductsTableAdapter().GetData()
+			DataContext = dt(currentRecord)
+			comboBoxEdit1.ItemsSource = New nwindDataSetTableAdapters.CategoriesTableAdapter().GetData()
+		End Sub
+
+		Private Sub prevButton_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			currentRecord -= 1
+			UpdateData()
+			UpdateButtonState()
+		End Sub
+		Private Sub nextButton_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			currentRecord += 1
+			UpdateData()
+			UpdateButtonState()
+		End Sub
+		Private Sub UpdateData()
+			DataContext = dt(currentRecord)
+		End Sub
+		Private Sub UpdateButtonState()
+			prevButton.IsEnabled = currentRecord <> 0
+			nextButton.IsEnabled = currentRecord <> dt.Rows.Count - 1
+		End Sub
+	End Class
+End Namespace
