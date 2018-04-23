8a80687b91623786cc8a3853382bb0e7341e589f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXEditors_DataBinding/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace DXEditors_DataBinding {
+    public partial class Window1 : Window {
+        DXEditors_DataBinding.nwindDataSet.ProductsDataTable dt;
+        int currentRecord;
+        public Window1() {
+            InitializeComponent();
+            currentRecord = 0;
+            dt = new nwindDataSetTableAdapters.ProductsTableAdapter().GetData();
+            DataContext = dt[currentRecord];
+            comboBoxEdit1.ItemsSource = 
+                new nwindDataSetTableAdapters.CategoriesTableAdapter().GetData();
+        }
+
+        private void prevButton_Click(object sender, RoutedEventArgs e) {
+            currentRecord--;
+            UpdateData();
+            UpdateButtonState();
+        }
+        private void nextButton_Click(object sender, RoutedEventArgs e) {
+            currentRecord++;
+            UpdateData();
+            UpdateButtonState();
+        }
+        private void UpdateData() {
+            DataContext = dt[currentRecord];
+        }
+        private void UpdateButtonState() {
+            prevButton.IsEnabled = currentRecord != 0;
+            nextButton.IsEnabled = currentRecord != dt.Rows.Count - 1;
+        }
+    }
+}
